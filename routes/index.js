45d0b7d3ac24a8c297be58c27c72b09615afdf21
Julien Goudard <juliengoudard.g@gmail.com>
2023-04-28 17:04:56 +0200

    route byCategories

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,21 @@ router.get("/movies", (req, res) => {
     });
 });
 
+router.get("/genre", (req, res) => {
+  fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+  .then((response) => response.json())
+  .then(data => {
+    res.json({genres: data.genres})
+  })
+})
+
+router.post("/byCat", (req, res) => {
+  console.log('reqBack:', req)
+  fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${req.body.genreId}&api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    res.json({movies: data.results})
+    console.log(data.results)
+  })
+})
 module.exports = router;
